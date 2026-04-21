3f39ed573f3d1619381bd1f959a998698c3c6108
nguynkhn <nguynkhan479@gmail.com>
2026-04-22 05:32:31 +0700

    v1.2

--- studotools.user.js
+++ studotools.user.js
@@ -1,183 +1,256 @@
 // ==UserScript==
 // @name         Studotools
 // @namespace    http://github.com/nguynkhn/studotools
-// @version      1.1
-// @description  Userscript for document downloading
+// @version      1.2
+// @description  Userscript for downloading document
 // @author       nguynkhn
 // @match        *://*.studeersnel.nl/nl/document/*/*
 // @match        *://*.studocu.com/en/document/*/*
 // @match        *://*.studocu.id/id/document/*/*
 // @match        *://*.studocu.vn/vn/document/*/*
+// @match        *://*.scribd.com/doc/*/*
 // @match        *://*.scribd.com/document/*/*
 // @match        *://*.scribd.com/presentation/*/*
 // @icon         https://cdn-icons-png.flaticon.com/512/14964/14964744.png
 // @grant        GM_cookie
 // @grant        GM_addStyle
 // @grant        unsafeWindow
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const DOWNLOAD_BUTTON_CLASS = 'studotools-download-btn';
-    const PRINTING_CLASS = 'studotools-printing';
-    let downloadButton;
-    const sources = [
+    const DOCUMENT_CONTAINER_ID = 'studotools-document-container';
+    const DOWNLOAD_BUTTON_ID = 'studotools-download-button';
+    const DOWNLOAD_BUTTON_TEXTS = {
+        idle: 'Download as PDF',
+        fetching_document: (loadedPages, totalPages) => `Fetching document (${loadedPages}/${totalPages})`,
+        loading_images: 'Loading images...',
+        loading_fonts: 'Loading fonts...',
+        downloading: 'Downloading...',
+    };
+    const DOCUMENT_SOURCES = [
         // Studocu
         {
-            urlRegex: /(^|\.)((studeersnel\.nl)|(studocu\.(com|id|vn)))$/i,
-            buttonContainerClass: '.TopbarActions_secondary-actions-wrapper__4u75_',
-            buttonClass: '.Button_button__88E9y',
+            domainNames: ['studeersnel.nl', 'studocu.com', 'studocu.id', 'studocu.vn'],
             blockCookie: 'sd_docs',
-            fetchDocument: async () => {
-                const { documentAccess, pageDataList } = unsafeWindow.__NEXT_DATA__.props.pageProps;
+            buttonContainerSelector: '.TopbarActions_secondary-actions-wrapper__4u75_',
+            buttonSelector: '.Button_button__88E9y',
+            fetchDocument: async (progressUpdater) => {
+                const { documentAccess, pageDataList, seoData } = unsafeWindow.__NEXT_DATA__.props.pageProps;
                 const { url, objectKey, signedQueryParams } = documentAccess;
                 const params = signedQueryParams.global;
 
-                const pageContent = await Promise.all(pageDataList.map(async pageData => {
-                    const { pageNumber, pageHtmlWrapper } = pageData;
-                    const pageUrl = `${url}${objectKey}${pageNumber}.page${params}`;
+                progressUpdater.setTotalPages(pageDataList.length);
+                const pageRequests = pageDataList.map(async ({ pageHtml, pageNumber, pageHtmlWrapper }) => {
+                    if (!pageHtml) {
+                        const pageUrl = `${url}${objectKey}${pageNumber}.page${params}`;
+                        const backgroundFile = `bg${pageNumber.toString(16)}.png`;
+                        const backgroundUrl = `${url}${backgroundFile}${params}`;
 
-                    const backgroundFile = `bg${pageNumber.toString(16)}.png`;
-                    const backgroundUrl = `${url}${backgroundFile}${params}`;
+                        const pageResponse = await fetch(pageUrl);
+                        const pageResponseText = await pageResponse.text();
 
-                    const pageResponse = await fetch(pageUrl);
-                    const pageText = await pageResponse.text();
-                    const pageContent = pageText.replaceAll(backgroundFile, backgroundUrl);
+                        pageHtml = pageResponseText.replace(backgroundFile, backgroundUrl);
+                    }
 
-                    return `${pageHtmlWrapper}${pageContent}</div>`;
-                }));
+                    progressUpdater.increaseLoadedPage();
+                    return `${pageHtmlWrapper}${pageHtml}</div>`;
+                });
+                const pageContents = await Promise.all(pageRequests);
 
-                const firstImageElement = document.querySelector('div[data-page-index="0"] img');
-                const scale = firstImageElement.naturalWidth / firstImageElement.clientWidth;
+                const firstPage = document.querySelector('div[data-page-index="0"] .page-content');
+                const firstImage = firstPage.querySelector('img.bi');
+                const isHidden = firstPage.style.display == 'none';
+                if (isHidden) {
+                    firstPage.style.display = 'block';
+                }
 
-                return `
-<div class="p2hv" style="transform:scale(${scale});transform-origin:top left;">
-    <div id="page-container">${pageContent.join('')}</div>
+                const scaleX = firstImage.naturalWidth / firstImage.offsetWidth;
+                const scaleY = firstImage.naturalHeight / firstImage.offsetHeight;
+                if (isHidden) {
+                    firstPage.style.display = 'none';
+                }
+
+                const documentContent = `
+<div class="p2hv" style="transform:scale(${scaleX},${scaleY});transform-origin:top left;">
+  <div id="page-container">${pageContents.join('')}</div>
 </div>
 `;
+                return {
+                    title: seoData.originalTitle,
+                    content: documentContent,
+                    width: firstImage.naturalWidth,
+                    height: firstImage.naturalHeight,
+                };
             },
         },
         // Scribd
         {
-            urlRegex: /(^|\.)scribd\.com$/i,
-            buttonContainerClass: '._12sL1I',
-            buttonClass: '.ButtonCore-module_wrapper_MkTb9s',
-            fetchDocument: async () => {
-                const { pages } = unsafeWindow.docManager;
+            domainNames: ['scribd.com'],
+            buttonContainerSelector: '._12sL1I',
+            buttonSelector: '.ButtonCore-module_wrapper_MkTb9s',
+            fetchDocument: async (progressUpdater) => {
+                const { pages, firstVisiblePage } = unsafeWindow.docManager;
 
-                const pageContent = await Promise.all(Object.entries(pages).map(async ([ pageNo, pageData ]) => {
+                const pageList = Object.values(pages)
+                progressUpdater.setTotalPages(pageList.length);
+                const pageRequests = pageList.map(async pageData => {
                     const { contentUrl, containerElem, origWidth, origHeight } = pageData;
 
                     const pageResponse = await fetch(contentUrl);
-                    const pageCode = await pageResponse.text();
-                    const pageEscapedContent = pageCode.replace(`window.page${pageNo}_callback([`, '')
-                        .replace(/]\);\s*$/, '');
-                    const pageContent = JSON.parse(pageEscapedContent)
-                        .replace(/orig="http:\/\/html\.scribd\.com/g, 'style="display: block;" src="https://html.scribdassets.com');
+                    const pageResponseText = await pageResponse.text();
 
-                    const newContainer = containerElem.cloneNode();
-                    newContainer.innerHTML = pageContent;
-                    newContainer.style.width = `${origWidth}px`;
-                    newContainer.style.height = `${origHeight}px`;
+                    const pageEscapedContent = pageResponseText.replace(/^[^(]*\((.*)\);?$/, '$1');
+                    const pageContent = JSON.parse(pageEscapedContent)[0].replace(
+                        /orig="http:\/\/html\.scribd\.com/g,
+                        'style="display: block;" src="https://html.scribdassets.com'
+                    );
 
-                    return newContainer.outerHTML;
-                }));
+                    const pageContainer = containerElem.cloneNode();
+                    pageContainer.innerHTML = pageContent;
+                    pageContainer.style.width = `${origWidth}px`;
+                    pageContainer.style.height = `${origHeight}px`;
 
-                return pageContent.join('');
+                    progressUpdater.increaseLoadedPage();
+                    return pageContainer.outerHTML;
+                });
+                const pageContents = await Promise.all(pageRequests);
+
+                const hydrationTag = document.querySelector('script[data-hypernova-key="doc_page"]');
+                const { wordDocument } = JSON.parse(hydrationTag.textContent.replace(/<!--|-->/g, ''));
+
+                return {
+                    title: wordDocument.title,
+                    content: pageContents.join(''),
+                    width: firstVisiblePage.origWidth,
+                    height: firstVisiblePage.origHeight,
+                };
             },
         },
     ];
-    const source = sources.find(source => source.urlRegex.test(location.hostname));
+
+    const domainName = location.hostname.split('.').slice(-2).join('.');
+    const documentSource = DOCUMENT_SOURCES.find(source => source.domainNames.includes(domainName));
+    let documentInfo;
 
     async function downloadDocument() {
-        downloadButton.textContent = 'Downloading...';
-
-        if (!document.querySelector(`.${PRINTING_CLASS}`)) {
-            const documentContent = await source.fetchDocument();
-
-            const printingElement = document.createElement('div');
-            printingElement.classList.add(PRINTING_CLASS);
-            printingElement.innerHTML = documentContent;
-            document.body.appendChild(printingElement);
-
-            const imageElements = Array.from(printingElement.querySelectorAll('img'));
-            await Promise.all(imageElements.map(imageElement => new Promise(resolve => {
-                if (imageElement.complete) {
-                    resolve();
-                    return;
-                }
-
-                imageElement.addEventListener('load', resolve);
-                imageElement.addEventListener('error', resolve);
-            })));
-
-            await document.fonts?.ready;
-            await new Promise(r => requestAnimationFrame(r));
-        }
-
-        window.print();
-    }
-
-    function createDownloadButton() {
-        const buttonContainer = document.querySelector(source.buttonContainerClass);
-        if (!buttonContainer || buttonContainer.querySelector(`.${DOWNLOAD_BUTTON_CLASS}`)) {
+        const downloadButton = document.getElementById(DOWNLOAD_BUTTON_ID);
+        if (!downloadButton) {
             return;
         }
 
-        if (!downloadButton) {
-            downloadButton = buttonContainer.querySelector(source.buttonClass)?.cloneNode();
-            if (!downloadButton) {
-                return;
-            }
+        downloadButton.disabled = true;
+        if (!documentInfo || !document.getElementById(DOCUMENT_CONTAINER_ID)) {
+            const progressUpdater = {
+                loadedPages: 0, totalPages: 0,
+                setTotalPages: function (totalPages) {
+                    this.totalPages = totalPages;
+                    downloadButton.textContent = DOWNLOAD_BUTTON_TEXTS.fetching_document(
+                        this.loadedPages, this.totalPages
+                    );
+                },
+                increaseLoadedPage: function () {
+                    ++this.loadedPages;
+                    downloadButton.textContent = DOWNLOAD_BUTTON_TEXTS.fetching_document(
+                        this.loadedPages, this.totalPages
+                    );
+                },
+            };
+            documentInfo = await documentSource.fetchDocument(progressUpdater);
 
-            downloadButton.classList.add(DOWNLOAD_BUTTON_CLASS);
-            downloadButton.textContent = 'Download as PDF';
-            downloadButton.addEventListener('click', downloadDocument);
-            downloadButton.disabled = false;
-            downloadButton.style.pointerEvents = 'auto';
-            downloadButton.style.cursor = 'pointer';
+            const documentContainer = document.createElement('div');
+            documentContainer.id = DOCUMENT_CONTAINER_ID;
+            documentContainer.innerHTML = documentInfo.content;
+
+            const styleElement = document.createElement('style');
+            styleElement.textContent = `
+#${DOCUMENT_CONTAINER_ID} {
+  visibility: hidden;
+}
+
+@page {
+  margin: 0;
+  size: ${documentInfo.width}px ${documentInfo.height}px;
+}
+
+@media print {
+  body > *:not(#${DOCUMENT_CONTAINER_ID}) {
+    display: none;
+  }
+
+  #${DOCUMENT_CONTAINER_ID}, #${DOCUMENT_CONTAINER_ID} * {
+    visibility: visible;
+    box-shadow: none;
+    page-break-after: always;
+    break-after: always;
+    border: none;
+  }
+}
+`;
+            documentContainer.append(styleElement);
+            document.body.append(documentContainer);
+
+            const images = Array.from(documentContainer.querySelectorAll('img'));
+            const imagePromises = images.map(image => new Promise(resolve => {
+                if (image.complete) {
+                    resolve();
+                    return;
+                }
+                image.onload = image.onerror = resolve;
+            }));
+
+            downloadButton.textContent = DOWNLOAD_BUTTON_TEXTS.loading_images;
+            await Promise.all(imagePromises);
+
+            downloadButton.textContent = DOWNLOAD_BUTTON_TEXTS.loading_fonts;
+            await document.fonts?.ready;
+            await new Promise(resolve => requestAnimationFrame(() => requestAnimationFrame(resolve)));
         }
+
+        downloadButton.textContent = DOWNLOAD_BUTTON_TEXTS.downloading;
+        const originalTitle = document.title;
+        document.title = documentInfo.title;
+        unsafeWindow.print();
+
+        document.title = originalTitle;
+        downloadButton.disabled = false;
+        downloadButton.textContent = DOWNLOAD_BUTTON_TEXTS.idle;
+    }
+
+    function createDownloadButton() {
+        if (document.getElementById(DOWNLOAD_BUTTON_ID)) {
+            return;
+        }
+
+        const buttonContainer = document.querySelector(documentSource.buttonContainerSelector);
+        const pseudoDownloadButton = buttonContainer?.querySelector(documentSource.buttonSelector);
+        const downloadButton = pseudoDownloadButton?.cloneNode();
+        if (!downloadButton) {
+            return;
+        }
+
+        downloadButton.id = DOWNLOAD_BUTTON_ID;
+        downloadButton.textContent = DOWNLOAD_BUTTON_TEXTS.idle;
+        downloadButton.disabled = false;
+        downloadButton.style.pointerEvents = 'auto';
+        downloadButton.style.cursor = 'pointer';
+        downloadButton.onclick = downloadDocument;
 
         buttonContainer.prepend(downloadButton);
     }
 
-    window.addEventListener('load', () => {
-        if (source.blockCookie) {
-            GM_cookie.delete({ name: source.blockCookie });
+    unsafeWindow.addEventListener('DOMContentLoaded', () => {
+        if (!documentSource) {
+            return;
         }
 
-        GM_addStyle(`
-.${PRINTING_CLASS} {
-    display: none;
-}
+        if (documentSource.blockCookie) {
+            GM_cookie.delete({ name: documentSource.blockCookie });
+        }
 
-@media print {
-    @page {
-        margin: 0;
-        size: auto;
-    }
-    .${PRINTING_CLASS} {
-        display: block !important;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-    .${PRINTING_CLASS}, .${PRINTING_CLASS} * {
-        margin: 0;
-        visibility: visible;
-        box-shadow: none;
-        page-break-after: always;
-        break-after: always;
-        border: none;
-    }
-    body * {
-        visibility: hidden;
-    }
-}
-`);
-        const observer = new MutationObserver(createDownloadButton);
-        observer.observe(document.body, { childList: true, subtree: true });
-        window.addEventListener('afterprint', () => { downloadButton.textContent = 'Download as PDF' });
+        const domObserver = new MutationObserver(createDownloadButton);
+        domObserver.observe(document.body, { childList: true, subtree: true });
     });
 })();
